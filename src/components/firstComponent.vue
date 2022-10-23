76e98628bd3dffaa1cff6ab19c05ec9eaabe9013
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2022-10-24 01:14:10 +0900

    Add : firstComponent.vue

---
+++ src/components/firstComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>hedaer</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        logNum: function() {
+            console.log("10")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
