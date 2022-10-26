1c57e32703b01ee94442ad4d5a5ad7e50816d244
JeongTaehwan <wjdxoghks11222@naver.com>
2022-10-26 22:44:21 +0900

    Add : 컴포넌트 등록

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    {{str}}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
-  data: function() {
+  data: function () {
     return {
-      str: "hi"
-    }
-  }
-}
+      str: "hi",
+    };
+  },
+  components: {
+    "app-header": AppHeader,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
